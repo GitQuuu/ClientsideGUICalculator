0a3320815dc003adc682881c3e04b41a1539f77a
Qu <Quan0027@elec.techcollege.dk>
2020-03-04 09:06:19 +0100

    Copy JS script to TS

--- ClientsideGUICalculator/Scripts/Script.ts
+++ ClientsideGUICalculator/Scripts/Script.ts
@@ -1,2 +1,14 @@
-﻿function add(){
+﻿function dis(val) {
+    document.getElementById("Result").value += val;
+    console.log(val);
 }
+
+function solve() {
+    var x = document.getElementById("Result").value;
+    var y = eval(x);
+    document.getElementById("Result").value = y;
+}
+
+function clr() {
+    document.getElementById("Result").value = "";
+} 
